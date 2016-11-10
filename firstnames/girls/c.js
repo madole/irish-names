3c279030b5ef88fb5f86219cf2ca95ce34f0a25d
Andrew McDowell <madoliole@gmail.com>
2016-11-11 10:22:35 +1100

    Cs

---
+++ firstnames/girls/c.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'Carmel': 'Cairméal',
+    'Carol': 'Caral',
+    'Caroline': 'Cearúilín',
+    'Catherine': 'Caitríona',
+    'Cecelia': 'Síle', 
+    'Celene': 'Ceilín',
+    'Charlotte': 'Séarlait',
+    'Christina': 'Cristíona',
+    'Clara': 'Sorcha',
+    'Clare': 'Clár',
+    'Cloda': 'Clóda',
+    'Colma': 'Colma',
+    'Cora': 'Córa'
+}
