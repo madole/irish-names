58a68619ac50785837a5617acfa9b40d87aaa40d
Andrew McDowell <madoliole@gmail.com>
2016-11-17 15:58:31 +1100

    adding L

---
+++ firstnames/boys/l.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'Lambert': 'Laimbeart',
+    'Laserian': ['Laisrian', 'Laisréin'],
+    'Laughlin': 'Lochlainn',
+    'Laurence': 'Labhrás',
+    'Leslie': 'Leaslaoi',
+    'Leo': 'Leon',
+    'Leonard': 'Lionard',
+    'Lewis (Lewy)': 'Lughaidh',
+    'Loman': 'Lomán',
+    'Lonan': 'Lonán',
+    'Lorcan': 'Lorcán',
+    'Lucius': 'Laoiseach',
+    'Luke': 'Lúcás',
+    'Lysagh': 'Laoiseach'
+}
---
+++ firstnames/girls/l.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Lasarina': 'Lasairíona',
+    'Laura': 'Lára',
+    'Lelia': 'Lile',
+    'Lena': 'Eibhlín',
+    'Lil': 'Lil',
+    'Lile': 'Lile',
+    'Lizzie': 'Eilís',
+    'Lorna': 'Lorna',
+    'Louisa': 'Labhaoise',
+    'Lucy': 'Laoiseach'
+}
